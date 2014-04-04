721d267d82742d0afd849ae08834efa0453f8779
Arturo Pacifico Griffini <pacifico.arturo@gmail.com>
2014-04-04 11:14:06 -0700

    Initial Commit

---
+++ VendiMobile/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  VendiMobile
+//
+//  Created by APG on 04/04/14.
+//  Copyright (c) 2014 Vendi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "VendiAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([VendiAppDelegate class]));
+    }
+}
---
+++ VendiMobileTests/VendiMobileTests.m
@@ -0,0 +1,34 @@
+//
+//  VendiMobileTests.m
+//  VendiMobileTests
+//
+//  Created by APG on 04/04/14.
+//  Copyright (c) 2014 Vendi. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface VendiMobileTests : XCTestCase
+
+@end
+
+@implementation VendiMobileTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
